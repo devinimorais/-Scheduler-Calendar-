c5627145f48495e3c857a68ced0fefa0224434f2
viniciusDevsmink <vinicius.m@smink.com.br>
2024-11-27 10:50:14 -0300

    staring scheduler calendar

---
+++ src/pages/login/login.tsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+
+function Login() {
+
+
+    return (
+        <div>
+            <div className='bg-slate-950'>
+                ahh</div>
+        </div>
+    );
+}
+
+export default Login;
