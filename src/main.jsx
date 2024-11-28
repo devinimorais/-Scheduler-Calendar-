bdbf13c75b57687bdb4c0511abd9aebcaa813906
Jordan Feliphe <165801343+JordanFeliphe@users.noreply.github.com>
2024-11-27 21:05:50 -0300

    codigo funcionando

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import Login from './pages/login/login'
-import Scheduleduler from './pages/scheduleduler/Scheduleduler'
-import "./index.css";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Login from './pages/login/Login';
+import Scheduleduler from './pages/scheduleduler/Scheduleduler';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Login />
+  },
+
+  {
+    path: '/Scheduledule',
+    element: <Scheduleduler/>
+  }
+]);
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Login />
-    <Scheduleduler />
+    <RouterProvider router={router} />
   </StrictMode>
-)
+);
--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,75 +1,76 @@
 import React from "react";
 import { RiLockPasswordFill, RiUser3Fill, RiMailFill } from "react-icons/ri";
+import { FiUser } from "react-icons/fi";
+import { BiSolidLockAlt } from "react-icons/bi";
 
 const Login: React.FC = () => {
   return (
     <div
-      className="relative flex items-center justify-center min-h-screen bg-gradient-to-b from-black to-blue-800 px-4 sm:px-0"
+      className="relative flex items-center justify-center min-h-screen bg-gradient-to-b from-[#0e069d] to-[#74a8e6] px-4 sm:px-0"
     >
-      {/* Overlay */}
+
       <div className="absolute inset-0 bg-black/50"></div>
 
-      {/* Login form */}
-      <div className="relative bg-white border border-black backdrop-blur-md p-6 rounded-lg shadow-[0_0_15px_rgba(0,0,0,0.8)] max-w-sm w-full z-10">
+
+      <div className="relative bg-transparent  p-6 rounded-lg max-w-sm w-full  shadow-[0_30px_100px_rgba(0,0,0,0.7)] ">
         <div className="flex justify-center mb-4">
           <div className="bg-[#0442c5] p-4 rounded-full">
             <RiUser3Fill className="h-8 w-8 text-white" />
           </div>
         </div>
         <form>
-          {/* Email input */}
+
           <div className="mb-4">
             <label
               htmlFor="email"
-              className="block text-gray-700 text-sm text-[#0442c5] font-medium mb-2"
+              className="block font-medium mb-2 text-white"
             >
-              Email ID
+              Username
             </label>
-            <div className="flex items-center border border-white rounded-md focus-within:ring-2 focus-within:ring-[#313e22]">
+            <div className="flex items-center border border-white rounded-xl focus-within:ring-2 ">
               <div className="p-2">
-                <RiMailFill className="h-6 w-6 text-[#0442c5]" />
+                <FiUser className="h-6 w-6 text-white " />
               </div>
               <input
                 type="email"
                 id="email"
-                placeholder="Enter your email"
-                className="w-full px-4 py-2 focus:outline-none text-black bg-transparent"
+                placeholder="username"
+                className="w-full px-4 py-2 focus:outline-none bg-transparent"
               />
             </div>
           </div>
 
-          {/* Password input */}
+
           <div className="mb-4">
             <label
               htmlFor="password"
-              className="block text-gray-700 text-sm text-[#0442c5] font-medium mb-2"
+              className="block text-white font-medium mb-2"
             >
               Password
             </label>
-            <div className="flex items-center border border-white rounded-md focus-within:ring-2 focus-within:ring-[#313e22]">
+            <div className="flex items-center border border-white rounded-xl focus-within:ring-2 focus-within:ring-[#313e22]">
               <div className="p-2">
-                <RiLockPasswordFill className="h-6 w-6 text-[#0442c5]" />
+                <BiSolidLockAlt className="h-6 w-6 text-white " />
               </div>
               <input
                 type="password"
                 id="password"
-                placeholder="Enter your password"
+                placeholder=" password"
                 className="w-full px-4 py-2 focus:outline-none text-black bg-transparent"
               />
             </div>
           </div>
 
-          {/* Remember me and forgot password */}
+
           <div className="flex items-center justify-end mb-4">
-            <a href="#" className="text-sm text-[#0442c5] hover:underline">
+            <a href="#" className="text-sm text-white hover:underline">
               Forgot Password?
             </a>
           </div>
 
-          {/* Submit button */}
           <button
             type="submit"
-            className="w-full bg-[#0442c5] text-white py-2 px-4 rounded-md hover:bg-[#96cafb] transition-colors"
+            className="w-full text-white py-2 px-6 rounded-[20px] bg-gradient-to-r from-[#0400ff] to-[#4ce3f7] bg-[length:100%_auto] transition-all duration-500 ease-in-out hover:bg-[length:200%_auto] hover:bg-right focus:outline-none shadow-lg animate-pulse512"
           >
             Login
           </button>
